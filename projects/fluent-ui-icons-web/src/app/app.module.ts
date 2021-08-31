c7c45f3827c905f83c7d644ad0f1f7dd84c746d9
Benny Megidish <bennymegk@gmail.com>
2021-09-01 01:32:03 +0300

    feat: cleaned site tooltip

--- projects/fluent-ui-icons-web/src/app/app.module.ts
+++ projects/fluent-ui-icons-web/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
 import { IconsFilterPipe } from './pipes/icons-filter.pipe';
 
@@ -19,6 +20,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    MatTooltipModule,
     MatInputModule,
     MatButtonModule,
     LazyLoadImageModule,
